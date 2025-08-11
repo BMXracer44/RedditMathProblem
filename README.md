267e9d7f5480f922fe0ed9f09e38fc473600fb91
BMXracer44 <fergusalex444@gmail.com>
2025-08-11 11:35:30 -0500

    Solution

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RedditMathProblem
 This is a simple program to find the solution to a random math problem I found on Reddit
+
+There was a reddit post that was asking how a 5th grader was supposed to solve this problem:
+
+Digits 1-9 are used to solve abc * de = fghi and no digit can be used more than once
+
+I agree, it is a pretty complex math problem for a 5th grader. I could've solved it on my own, but felt like it would be easier to let the computer do it for me, so I made this program to solve it! 
---
+++ redditProblem.cpp
@@ -0,0 +1,43 @@
+# include <iostream>
+# include <cstdlib>
+
+int main(){
+    bool solved = false;
+    while(!solved){
+        int firstNum = rand() % 900 + 100;
+        int secondNum = rand() % 90 + 10;
+        int equals = firstNum * secondNum;
+
+        //Digits of each number
+        int digits[9] = {firstNum / 100, (firstNum / 10) % 10, (firstNum % 100) % 10, secondNum / 10, 
+        secondNum % 10, equals / 1000, (equals / 100) % 10, (equals / 10) % 10, equals % 10};
+
+        if(equals < 1000 || equals > 9999){
+            continue;
+        }
+
+        solved = true;
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(i != j){
+                    if(digits[i] == digits[j] || digits[i] == 0 || digits[j] == 0){
+                        solved = false;
+                        break;
+                    }
+                }
+            }
+            if(!solved){
+                break;
+            }
+        }
+        if(solved){
+            std::cout << "Found solution: ";
+            std::cout << digits[0] << digits[1] << digits[2] << " x " << digits[3] << digits[4] << " = "
+            << digits[5] << digits[6] << digits[7] << digits[8];
+        }
+    }
+
+    
+
+}
